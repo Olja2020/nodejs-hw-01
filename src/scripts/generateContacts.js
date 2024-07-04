43497e33aa94ed8515d7dd3e3219552b3887c33c
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-07-04 23:57:31 +0300

    add

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,22 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+        const array=[];
+        for(let i=0; i<=number; i++){
+          
+          const contact = JSON.stringify(createFakeContact(number));
+          array.push(contact);
+          console.log(array);
+        }
+
+        try {
+          await fs.appendFile(PATH_DB, array, 'utf8');
+          console.log('Дані успішно додані до файлу.');
+        } catch (err) {
+          console.error('Помилка додавання даних до файлу:', err);
+        }
+      };
 
 generateContacts(5);
