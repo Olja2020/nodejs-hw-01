7f33fa69a1fd90ef81752efb789eda35793057f9
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-07-05 14:40:29 +0300

    add

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -2,20 +2,24 @@ import { PATH_DB } from '../constants/contacts.js';
 import fs from 'fs/promises';
 import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {
-  const array = [];
-  for (let i = 0; i < number; i++) {
-    const contact = JSON.stringify(createFakeContact(number));
-    array.push(contact);
-    console.log(array);
-  }
-
+export const generateContacts = async (number) => {
   try {
-    await fs.appendFile(PATH_DB, array, 'utf8');
-    console.log('Дані успішно додані до файлу.');
-  } catch (err) {
-    console.error('Помилка додавання даних до файлу:', err);
+    
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    const contacts = JSON.parse(data);
+    console.log(contacts);
+    
+    for (let i = 0; i < number; i++) {
+      contacts.push(createFakeContact());
+      
+    }
+
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+
+    console.log(`Успішно додано ${number} нових контактів у файл ${PATH_DB}`);
+  } catch (error) {
+    console.error('Помилка під час зчитування або запису файлу:', error);
   }
 };
 
-generateContacts(2);
+generateContacts(3);
